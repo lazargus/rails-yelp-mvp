d443918e58e1098de22c76738fa3c7e3d0ec9623
Augustin Lazarus <augustin.lazarus@free.fr>
2020-05-13 14:38:26 +0200

    2nd challenge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+lotus = { name: 'lotus', address: 'avenue du roule', category: 'chinese' }
+livio = { name: 'livio', address: 'pont de neuilly', category: 'italian' }
+ichiban = { name: 'ichiban', address: 'rue des dames', category: 'japanese' }
+daroco = { name: 'daroco', address: 'maison de la radio', category: 'italian' }
+riboute = { name: 'riboute', address: 'rue des batignolles', category: 'french' }
+
+[lotus, livio, ichiban, daroco, riboute ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
